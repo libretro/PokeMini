1dfb4cebcdefcacb5048cdc4c3c2f41c862963bf
twinaphex <libretro@gmail.com>
2021-09-22 16:20:40 +0200

    Get rid of some functions we don't need for the libretro port

--- source/CommandLine.h
+++ source/CommandLine.h
@@ -23,22 +23,6 @@
 #include <stdint.h>
 
 typedef struct {
-	char name[32];	// Flag name
-	int *ref;	// Reference pointer of the variable
-	int type;	// Type (Bool or Integer)
-	int numa;	// Minimum value for integer / Value to set
-	int numb;	// Maximum value for integer
-} TCommandLineCustom;
-
-enum {
-	COMMANDLINE_EOL,     // End-Of-List
-	COMMANDLINE_BOOL,    // Boolean
-	COMMANDLINE_INT,     // Integer, NumA = Minimum, NumB = Maximum
-	COMMANDLINE_INTSET,  // Set value (custom only), NumA = Value
-	COMMANDLINE_STR,     // String, NumA = String size minus NULL
-};
-
-typedef struct {
 	int forcefreebios;
 	char min_file[PMTMPV];
 	char bios_file[PMTMPV];
@@ -67,34 +51,12 @@ typedef struct {
 	uint32_t custompal[4];
 	int lcdcontrast;
 	int lcdbright;
-	const char *pokefile;
-	const char *conffile;
-	const TCommandLineCustom *confcustom;
 } TCommandLine;
 
 // Extern command line structure
 extern TCommandLine CommandLine;
 
-// Callbacks
-extern int (*PokeMini_PreConfigLoad)(const char *filename, const char *platcfgfile);
-extern int (*PokeMini_PostConfigSave)(int success, const char *filename, const char *platcfgfile);
-
-// Unknown key callback, return false to abort loading
-typedef int (*TCustomConfCallback)(char *key, char *value, const TCommandLineCustom *custom);
-
 // Process arguments parsing into the command line structure
 void CommandLineInit(void);
-int CommandLineArgs(int argc, char **argv, const TCommandLineCustom *custom);
-int CommandLineConfFile(const char *filename, const char *platcfgfile, const TCommandLineCustom *custom);
-int CommandLineConfSave(void);
-
-// Process custom config file
-int CustomConfFile(const char *filename, const TCommandLineCustom *custom, TCustomConfCallback unknown);
-int CustomConfSave(const char *filename, const TCommandLineCustom *custom, const char *description);
-
-// Write into a stream/string the command line help usage
-// PrintHelpUsageStr return number of bytes required for the output, "out" can be NULL
-void PrintHelpUsage(FILE *fout);
-int PrintHelpUsageStr(char *out);
 
 #endif
